e1df2f1fc21f0a88f26fcb38f6e4821d6f709dcc
NWJ9PB <helodibalado@gmail.com>
2021-10-21 22:43:37 +0800

    Create `localhostFlix`

---
+++ localhostFlix/listContents.php
@@ -0,0 +1,19 @@
+<?php
+// Load Essential loader
+
+require ('../vendor/autoload.php');
+require ('../settings/initialization.php');
+?>
+<?php
+
+$sql    = 'SELECT * FROM `app_localflix_contentlist`';
+$stmt   = $conn->prepare($sql);
+$stmt   -> execute();
+$result = $stmt->get_result();
+
+foreach($result as $row)
+{
+     echo "Show Name: ".$row['content_title']."<br> with ID of ".$row["content_id"]."<br>";
+}
+
+?>
