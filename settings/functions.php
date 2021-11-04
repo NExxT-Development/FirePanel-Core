854e63b6d75b78f1772e7c4bbbe7bd30302b7f08
NWJ9PB <helodibalado@gmail.com>
2021-11-04 14:53:25 +0800

    Upload File

--- settings/functions.php
+++ settings/functions.php
@@ -1,5 +1,8 @@
 <?php
-function getUserIP() {
+function getOriginIP($hash = FALSE) {
+    //? Load Global Variables
+    require('variables.php');
+    
     if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
         $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
         $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
@@ -9,14 +12,18 @@ function getUserIP() {
     $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
     $remote  = $_SERVER['REMOTE_ADDR'];
 
-    if(filter_var($client, FILTER_VALIDATE_IP)) {
+    if (filter_var($client, FILTER_VALIDATE_IP)) {
         $ip = $client;
     } elseif(filter_var($forward, FILTER_VALIDATE_IP)) {
         $ip = $forward;
     } else {
         $ip = $remote;
     }
-    return $ip;
+    if ($hash !== FALSE) {
+        return(sha1($ip . $SALTVALUE1));
+    } else {
+        return $ip;
+    }
 }
 
 function randbytes($len = 32, $cap = TRUE) {
