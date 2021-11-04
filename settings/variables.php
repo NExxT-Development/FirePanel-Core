5b365cf2164f37334583c5a73f3e26f6138e51e4
NWJ9PB <helodibalado@gmail.com>
2021-11-04 14:52:16 +0800

    Create File

---
+++ settings/variables.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ *  SkyPanel Global Variable Registry
+ * 
+ *  All variables used on this project are stored on this file 
+*/
+
+/*
+ ? --------------------------------------------------------------
+ * NAME:    SALTING VALUE 1
+ * TYPE:    STRING
+ ? --------------------------------------------------------------
+*/ 
+$SALTVALUE1 = "22719C10306EC6CE2374344FC845983CC4B5FD61D0B16F23B63E3D184D9E074900793214A15A7CA437E69801C94AB91D";
+// ? -------------------------------
