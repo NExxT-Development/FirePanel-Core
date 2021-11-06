405c2f58e6e9da3b69848eff92173b714ca29904
NWJ9PB <helodibalado@gmail.com>
2021-11-06 21:30:57 +0800

    APIv2 Specifications on Action

--- api/v2/flapwings/users/check_auth.php
+++ api/v2/flapwings/users/check_auth.php
@@ -1,7 +1,7 @@
 <?php
 // Load Essential loaders
-require ('../../../../vendor/autoload.php');
-require ('../../../../settings/initialization.php');
+require '../../../../vendor/autoload.php';
+require '../../../../settings/initialization.php';
 ?>
 <?php
 // Output
@@ -9,14 +9,29 @@ $json   = new stdClass();
 
 $json   = [
     "request" => [
-        "request_Status" => [
-            "status_result_http"    =>      "200",
-            "status_result_code"    =>      "OK",
+        "request_Status"        => [
+            "status_result_http"        =>      '200',
+            "status_result_code"        =>      'OK',
+        ],
+        "request_Output"        => [
+            "FLAP_ID"                   =>      strtoupper(session_id()),
+        ],
+        "request_Origin"        => [
+            "origin_IP"                 =>      strtoupper(getOriginIP(TRUE)),
+            "ND_OpenSessionID"          =>      OPENSESSIONID,
+        ],
+        "request_Reference"     => [
+            "referrence_ULID"           =>      $ulid,
+            "referrence_Snowflake"      =>      $snowflake->id(),
+        ],
+        "request_ServerInfo"    => [
+            "serverInfo_UUID"           =>      "DA5C708F-4EB6-4F4D-9C79-3E5D93FA71AC",
+            "serverInfo_Time"           =>      date("c"),
         ],
     ],
 ];
 
 
-$print = json_encode($json, JSON_PRETTY_PRINT);
-exit($print);
+$json_out = json_encode($json, JSON_PRETTY_PRINT);
+exit($json_out);
 ?>
